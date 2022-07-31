ef8e77bec70037ad1ba725c995df5e80fbb165e2
sam3 <cwm9889@gmail.com>
2022-07-31 02:58:07 -0400

    Add some classes

--- hangman.rb
+++ hangman.rb
@@ -3,18 +3,58 @@
 # Goal: Create hangman with ability to save and load states
 
 # somevar.methods - Object.instance_methods # to see unique methods
+# utilize private/public methods
+# utilize a module
+# instance_variables method
+# super
+# namespace with modules
+# constants
+# to_s method override 
+# use pry-byebug / binding.pry instead of just puts
 
 require 'pry-byebug'
 
-word_bank = File.readlines('google-10000-english-no-swears.txt')
-# word_bank = File.readlines('short_bank.txt')
-word = ''
-
-loop do
-  word = word_bank.sample.delete("\n")
-  break if word.length > 4
+module Swimmable
+  def swim
+    puts "Look! I can swim!"
+  end
 end
 
-p word
+module Serializable
+  def serialize
+    puts 'We can serialize data with this method.'
+  end
+end
 
-exit
+class WordGame
+  include Serializable
+end
+
+class Hangman < WordGame
+end
+
+class Player
+  include Swimmable
+end
+
+class Computer
+  def random_word
+    word_bank = File.readlines('google-10000-english-no-swears.txt')
+    
+    word = ''
+    loop do
+      word = word_bank.sample.delete("\n")
+      break if word.length > 4
+    end
+    word
+  end
+end
+
+
+jon = Player.new
+jon.swim
+
+game = Hangman.new
+game.serialize
+
+Hangman.new
